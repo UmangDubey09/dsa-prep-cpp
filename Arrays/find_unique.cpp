cf9629e78a7d06b5c52ccc8a6715b359074569dc
Umang <96.umang.dubey@gmail.com>
2026-03-24 01:16:31 +0530

    using xor

--- Arrays/find_unique.cpp
+++ Arrays/find_unique.cpp
@@ -6,7 +6,7 @@ Now, in the given array/list, 'M' numbers are present twice and one number is pr
 
 You need to find and return that number which is unique in the array/list.
 
- Note:
+Note:
 Unique element is always present in the array/list according to the given condition.
 Detailed explanation ( Input/output format, Notes, Images )
 Constraints :
@@ -21,7 +21,9 @@ Sample Output 1:
 1
 Explanation: The array is [2, 3, 1, 6, 3, 6, 2]. 
 Here, the numbers 2, 3, and 6 are present twice, and the number 1 is present only once. 
-So, the unique number in this array is 1. 
+So, the unique number in this array is 1.
+
+problem link :- https://www.naukri.com/code360/problems/find-unique_625159?leftPanelTabValue=PROBLEM
 */
 int findUnique(int *arr, int size)
 {
