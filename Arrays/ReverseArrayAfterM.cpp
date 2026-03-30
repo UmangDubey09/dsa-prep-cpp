f9155b2e072924a2afe7136aba4d9e01f3aba7ce
Umang <96.umang.dubey@gmail.com>
2026-03-31 00:49:17 +0530

    2pointerApproch

--- Arrays/ReverseArrayAfterM.cpp
+++ Arrays/ReverseArrayAfterM.cpp
@@ -9,6 +9,7 @@ based indexing so the subarray {5, 6} will be reversed and our
 output array will be {1, 2, 3, 4, 6, 5}.
 Note := We are following two pointer approach.
 Time Complexity: O(N), space complexity: O(1)
+Problem link :- https://www.naukri.com/code360/problems/reverse-the-array_1262298?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio3
 */
 
 void reverseArray(vector<int> &arr , int m) {
